bd80b4f29292ca4ff49d741f82e59c5813445b4f
Christian Lund <clund@stilmark.com>
2021-02-07 13:33:08 +0100

    Classes Request and String

---
+++ src/Request.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Stilmark\Parse;
+
+class Request
+{
+	private $Vars;
+	public static $serverVars = [
+		'useragent' => 'HTTP_USER_AGENT',
+		'referer' => 'HTTP_REFERER',
+		'method' => 'REQUEST_METHOD',
+		'ip' => 'REMOTE_ADDR',
+		'locale' => 'HTTP_ACCEPT_LANGUAGE',
+		'country' => 'HTTP_CF_IPCOUNTRY',
+	];
+
+	function __construct() {
+
+		// Overwrite Cloudflare IP with actual remote IP
+		if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+			$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+		}
+
+		foreach(self::$serverVars AS $static => $serverVar) {
+			$this->vars[$static] = isset($_SERVER[$serverVar]) ? $_SERVER[$serverVar]:null;
+		}
+
+		$this->vars = array_merge(parse_url(self::url()), $this->vars);
+
+	}
+
+	public static function init() {
+		 return new self;
+	}
+
+	public static function url() {
+		return 'http' . (isset($_SERVER['HTTPS']) && isset($_SERVER['HTTPS']) ? 's':'') . '://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	}
+
+    public static function __callStatic($name, $arguments)
+    {
+        $Request = self::init();
+        if (isset($Request->vars[$name])) {
+        	return $Request->vars[$name];
+        } else {
+        	return false;
+        }
+
+    }
+
+}
---
+++ src/String.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Stilmark\Parse;
+
+class String
+{
+
+}
