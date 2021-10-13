58a53ce549553863cac80136881a3436b0eecf95
Christian Lund <clu@brandhouse.com>
2021-10-13 12:49:01 +0200

    ResponseCodes

--- src/Route.php
+++ src/Route.php
@@ -10,12 +10,32 @@ class Route
 {
 
     const HANDLER_DELIMITER = '@';
+    public static $reponseCodes = [
+        200 => 'Ok',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        410 => 'Gone',
+        500 => 'Internal Server Error'
+    ];
 
 	public static function redirect($url)
 	{
 	    header('Location: '.filter_var($url, FILTER_SANITIZE_URL));
 	    exit;
 	}
+
+	public static function header($responseCode)
+    {
+        if (isset(self::$reponseCodes[$responseCode])) {
+            header('HTTP/1.0 '.$responseCode.' '.self::$reponseCodes[$responseCode]);
+        }
+    }
+
+    public static function errorResponse($responseCode, $message = '') {
+        self::header($responseCode);
+	    $message = (empty($message) && isset(self::$reponseCodes[$responseCode])) ? self::$reponseCodes[$responseCode]:$message;
+        return ['error' => $responseCode, 'message' => $message];
+    }
 
 	public static function dispatch()
 	{
@@ -34,12 +54,10 @@ class Route
         switch ($routeInfo[0]) {
 
 			case FastRoute\Dispatcher::NOT_FOUND:
-                header('HTTP/1.0 404 Not Found');
-                return ['error' => '404 Not Found'];
+			    return self::errorResponse(404);
 
 			case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-                header('HTTP/1.0 405 Method Not Allowed');
-                return ['error' => '405 Method Not Allowed'];
+                return self::errorResponse(405);
 
 			case FastRoute\Dispatcher::FOUND:
                 list($state, $handler, $vars) = $routeInfo;
@@ -48,11 +66,18 @@ class Route
                 $container = (isset($_ENV['NAMESPACE']) ? $_ENV['NAMESPACE'].'\\':'').$class;
                 $view = (new $container())->$method(...array_values($vars));
 
-			    return ['view' => $view, 'route' => $routeInfo];
+                if (isset($view['error'])) {
+                    if (!isset($view['message'])) {
+                        $view['message'] = '';
+                    }
+                    return self::errorResponse($view['error'], $view['message']);
+                } else {
+                    self::header(200);
+                    return $view;
+                }
 
             default:
-                header('HTTP/1.0 410 Gone');
-                return ['error' => '410 Gone'];
+                return self::errorResponse(410);
 
 		}
 	}
